6aa49be1182edf456c3b10cd766c9873e3c150d9
dipinkumar222 <dipinkumar222@gmail.com>
2024-05-27 20:42:06 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import LoginForm from './LoginForm'; // Importing LoginForm component
+
+function App() {
+  return (
+    <div className="App">
+      <LoginForm /> {/* Rendering the LoginForm component */}
+    </div>
+  );
+}
+
+export default App;
